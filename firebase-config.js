ba1fb6024610943d5912ebf215c5796c67f227c1
miraclejesupelumi123-bit <miraclejesupelumi123@gmail.com>
2026-06-27 16:13:46 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB5gaFlx4llGtJygmEt-awU-MUakEJE7AU",
+  authDomain: "egc---follow-up.firebaseapp.com",
+  projectId: "egc---follow-up",
+  storageBucket: "egc---follow-up.firebasestorage.app",
+  messagingSenderId: "48796498031",
+  appId: "1:48796498031:web:075b9f5b63ad3c3789759a"
+};
+
+const isConfigured = true;
+
+let db = null;
+
+if (isConfigured) {
+  firebase.initializeApp(firebaseConfig);
+  db = firebase.firestore();
+} else {
+  console.warn("Firebase not configured yet.");
+}
