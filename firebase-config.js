705b65b7b8d92810c05af964632e5dc27c948736
miraclejesupelumi123-bit <miraclejesupelumi123@gmail.com>
2026-06-27 16:52:27 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyB5gaFlx4llGtJygmEt-awU-MUakEJE7AU",
-    authDomain: "egc---follow-up.firebaseapp.com",
-    projectId: "egc---follow-up",
-    storageBucket: "egc---follow-up.firebasestorage.app",
-    messagingSenderId: "48796498031",
-    appId: "1:48796498031:web:075b9f5b63ad3c3789759a",
-    measurementId: "G-9TVKSSQ52D"
-  };
+  apiKey: "AIzaSyB5gaFlx4llGtJygmEt-awU-MUakEJE7AU",
+  authDomain: "egc---follow-up.firebaseapp.com",
+  projectId: "egc---follow-up",
+  storageBucket: "egc---follow-up.firebasestorage.app",
+  messagingSenderId: "48796498031",
+  appId: "1:48796498031:web:075b9f5b63ad3c3789759a"
+};
 
 const isConfigured = true;
 
@@ -17,4 +16,4 @@ if (isConfigured) {
   db = firebase.firestore();
 } else {
   console.warn("Firebase not configured yet.");
-} 
+}
